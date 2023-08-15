cbffc982e110e00ddb16fa3617d3ebccd1967ace
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-16 01:25:18 +0300

    commit

---
+++ DEPENDENCY_INVERSION_PRINCIPLE/Interface/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loose_Coupling_Principles
+{
+	public interface IMailService //BURADA DEPENDENCY_INVERSION PRENSİBİNE UYARAK BİR BAĞIMLILIK YERİNE BİRDEN ÇOK BAĞIMLILIĞA KAPI AÇMIŞ OLDUK.
+	{
+		public void Send(string to, string body, string mymail, string mymailpassword);
+	}
+}
